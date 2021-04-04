3cb3a42eb39c9333f4ad28f60e9056489ccb8f48
Aleks Devmcee <info@devmc.ee>
2021-04-04 08:03:20 +0300

    add order list

--- src/App.tsx
+++ src/App.tsx
@@ -4,22 +4,28 @@ import Button from './components/Button';
 import { NavBar } from './components/NavBar';
 import MainPage from './components/MainPage';
 import MenuItemButton from './components/MenuItemButton';
-import { MenuItemButtonType } from './types/button';
-import { CURRENCY } from './dataSets/CURRENCY';
-import {MENU} from './__mock_data__/MENU';
+import { MENU } from './__mock_data__/MENU';
+import { useSelector } from 'react-redux';
+import { AppState } from './redux/rootReducer';
 function App() {
 
 
-  const keyboard = MENU.map((item)=><MenuItemButton key={item.code} {...item} />);
+  const keyboard = MENU.map((item) => <MenuItemButton key={item.code} {...item} />);
+  const { order } = useSelector(({ order }: AppState) => order);
+  let orderList = [];
+  for (let item in order) {
+    orderList.push(<div>{item}:{order[item]}</div>)
+  }
+
   return (
     <div className="App">
 
       <div className="container">
         <MainPage
-          keyboard={<div>{keyboard}</div>} customer={<>Cutomer</>} order={<>cart</>} checkout={<>checkout</>} />
+          keyboard={<div>{keyboard}</div>} customer={<>Cutomer</>} order={<>{orderList}</>} checkout={<>checkout</>} />
       </div>
     </div>
   );
 }
 
-export default App; 
+export default App;
--- src/components/MenuItemButton/MenuItemButton.tsx
+++ src/components/MenuItemButton/MenuItemButton.tsx
@@ -1,32 +1,31 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { addToCartAction } from '../../redux/actions/orderActions';
 import { MenuItemButtonType } from '../../types/button';
-import {AppState} from '../../redux/rootReducer'
+import { AppState } from '../../redux/rootReducer'
 import './styles/MenuItemButton.css';
 
-const MenuItemButton = ({  classes = '', code, name, price, currencySymbol }: MenuItemButtonType): JSX.Element => {
+const MenuItemButton = ({ classes = '', code, name, price, currencySymbol }: MenuItemButtonType): JSX.Element => {
     const dispatch = useDispatch();
-    const {order} = useSelector(({order}: AppState) => order)
-    console.log('-',order);
-    
-    const itemQtyAddedText =  code in order ?   'x' +  order[code]: 0 ;
+    const { order } = useSelector(({ order }: AppState) => order);
+
+    const itemQtyAddedText = code in order ? 'x' + order[code] : 0;
     const clickHandler = (code: string) => dispatch(addToCartAction(code));
 
-    return <button onClick={()=>clickHandler(code)} className={'menu-item container ' + classes + (order[code] > 0? ' menu-item--active':'')}>
+    return <button onClick={() => clickHandler(code)} className={'menu-item container ' + classes + (order[code] > 0 ? ' menu-item--active' : '')}>
         <div className="menu-item__row row ">
             <div className="menu-item__number col s6">
-                {code} 
-            </div> 
+                {code}
+            </div>
             <div className="menu-item__name col s6">
                 {name}
-            </div> 
-        </div> 
+            </div>
+        </div>
         <div className="menu-item__row  menu-item__row--footer row">
             <div className="menu-item__price col s6">
                 {price + currencySymbol}
-            </div> 
-            <div className="menu-item__quantity col s6"> 
-                {itemQtyAddedText} 
+            </div>
+            <div className="menu-item__quantity col s6">
+                {itemQtyAddedText}
             </div>
         </div>
     </button>
