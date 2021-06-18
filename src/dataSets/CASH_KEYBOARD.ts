e7c1bb01095b13f67492aa194a428e35a7f28846
Aleks Devmcee <info@devmc.ee>
2021-06-18 22:54:15 +0300

    feat(cashKayboard): add dataset

---
+++ src/dataSets/CASH_KEYBOARD.ts
@@ -0,0 +1,52 @@
+import { KeyboardValue } from '../types/cashKeyBoard';
+
+export const CASH_KEYBOARD: KeyboardValue[] = [
+	{
+		name: '200€',
+		value: 200,
+	},
+	{
+		name: '100€',
+		value: 100,
+	},
+	{
+		name: '50€',
+		value: 50,
+	},
+	{
+		name: '20€',
+		value: 20,
+	},
+	{
+		name: '10€',
+		value: 10,
+	},
+	{
+		name: '5€',
+		value: 5,
+	},
+	{
+		name: '2€',
+		value: 2,
+	},
+	{
+		name: '1€',
+		value: 1,
+	},
+	{
+		name: '50s',
+		value: 0.5,
+	},
+	{
+		name: '20s',
+		value: 0.2,
+	},
+	{
+		name: '10s',
+		value: 0.1,
+	},
+	{
+		name: '5s',
+		value: 0.05,
+	},
+];
