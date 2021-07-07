f3173bc23cf68480fa0a7c38f24dc0236a575dad
Aleks Devmcee <info@devmc.ee>
2021-07-07 22:10:48 +0300

    refactor(KeyBoard): move to sets folder

--- src/components/sets/KeyBoard/KeyBoard.tsx
+++ src/components/sets/KeyBoard/KeyBoard.tsx
@@ -1,4 +1,4 @@
-import { MenuItemButtonType } from '../../types/button';
+import { MenuItemButtonType } from '../../../types/button';
 
 // Contains menu btns
 const KeyBoard: KeyBoard = ({ menuData, menuItemButton: MenuItemButton }: KeyboardProps) => {
--- src/components/sets/KeyBoard/stories/Keyboard.stories.tsx
+++ src/components/sets/KeyBoard/stories/Keyboard.stories.tsx
@@ -1,7 +1,7 @@
 import KeyBoard, { KeyboardProps } from '../KeyBoard';
 import { Meta, Story } from '@storybook/react/types-6-0';
-import { CURRENCY } from '../../../dataSets/CURRENCY';
-import { MenuItem } from '../../../components/buttons/MenuItemButton';
+import { CURRENCY } from '../../../../dataSets/CURRENCY';
+import { MenuItem } from '../../../buttons/MenuItemButton';
 export default {
     title: 'KeyBoard',
     component: KeyBoard,
