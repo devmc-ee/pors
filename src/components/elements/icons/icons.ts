79c5a24f6f68e45149100f3029d97b881bd8eac1
Aleks Devmcee <info@devmc.ee>
2021-07-09 22:06:02 +0300

    refactor: fix resources paths

--- src/components/elements/icons/icons.ts
+++ src/components/elements/icons/icons.ts
@@ -1,2 +1,2 @@
-export { ReactComponent as Bolt } from '../../assets/icons/paymentMethods/Bolt_Logo_green.svg';
-export { ReactComponent as Wolt } from '../../assets/icons/paymentMethods/wolt_logo.svg';
+export { ReactComponent as Bolt } from '../../../assets/icons/paymentMethods/Bolt_Logo_green.svg';
+export { ReactComponent as Wolt } from '../../../assets/icons/paymentMethods/wolt_logo.svg';
